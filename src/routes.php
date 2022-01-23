b98a3ab2e83a064d566638803a58df033e8be074
bimacoding <bimacoding@gmail.com>
2022-01-23 18:43:40 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,7 @@
 <?php
 Route::group(['middleware' => ['web','auth']], function() {
-
+    Route::get('/crudgenerator',function(){
+        return view('be.crudview.index');
+    });
+    Route::post('/crud/insert',[\Erendi\Crudgenerator\CrudController::class,'insert'])->name('crud.insert');
 });
